1a7d372fa5991f4f17b64580c8e914550565d3af
jonyalmeida <jony.figs@gmail.com>
2020-09-09 14:22:51 -0500

    Set '/' to render homepage.pug

--- averageReads/client-side/index.js
+++ averageReads/client-side/index.js
@@ -1,25 +1,21 @@
 const express = require("express");
 const path = require("path");
-// const cors = require('cors');
+const cors = require('cors');
+
 const app = express();
 
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
 //cors
-// app.use(cors({ origin: 'http://localhost:8080' }));
+app.use(cors({ origin: 'http://localhost:8080' }));
+
+// app.get("/", (req, res) => {
+//   res.render("start-page");
+// });
 
 app.get("/", (req, res) => {
-  res.render("start-page");
-});
-
-app.get("/log-in", (req, res) => {
   res.render("homepage");
-});
-
-
-app.get("/sign-up", (req, res) => {
-  res.render("sign-up");
 });
 
 //DB Populate Route
