c346185b1198a14498fe11f14e45cfa10bf0d8ed
Dan Black <danielb0318@gmail.com>
2020-09-09 17:07:57 -0400

    Adding logo

--- averageReads/client-side/index.js
+++ averageReads/client-side/index.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const path = require("path");
-// const cors = require('cors');
+const cors = require('cors');
+const fetch = require('node-fetch')
 const app = express();
 
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
-//cors
-// app.use(cors({ origin: 'http://localhost:8080' }));
 
-app.get("/", (req, res) => {
-  res.render("start-page");
-});
+const asyncHandler = (handler) => (req, res, next) =>
+    handler(req, res, next).catch(next);
+
+//cors
+app.use(cors({ origin: 'http://localhost:8080' }));
+
+app.get("/home", asyncHandler(async(req, res) => {
+  const result = await fetch('http://localhost:8080/books')
+  const { books } = await result.json()
+  const qoute = await fetch('https://opinionated-quotes-api.gigalixirapp.com/v1/quotes')
+    .then(qod => qod.json())
+  const qod = qoute.quotes[0]
+  console.log(books)
+  console.log(qod)
+  res.render("start-page", { books, qod });
+}));
 
 app.get("/log-in", (req, res) => {
   res.render("log-in");
