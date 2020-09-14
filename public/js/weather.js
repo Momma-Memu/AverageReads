86ab3bd9c145be01cc2685bce73e900e12988707
Miah Barnes <miahellenbarnes@gmail.com>
2020-09-14 09:28:37 -0400

    api fix final final final final

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,3 +1,12 @@
+navigator.geolocation.getCurrentPosition(async function(position) {
+    const lat = Number(position.coords.latitude.toString().slice(0, 6));
+    const lng = Number(position.coords.longitude.toString().slice(0, 6));
+
+    document.cookie = `lat=${lat}; max-age-in-seconds=259200`;
+    document.cookie = `lng=${lng}; max-age-in-seconds=259200`;
+
+})
+
 
 domManipulate()
 
@@ -84,56 +93,7 @@ navigator.geolocation.getCurrentPosition(async function(position) {
     const lat = Number(position.coords.latitude.toString().slice(0, 6));
     const lng = Number(position.coords.longitude.toString().slice(0, 6));
 
-    // cookie('lat', lat, { 'maxAge': 259200000 })
-    // cookie('lng', lng, { 'maxAge': 259200000 })
     document.cookie = `lat=${lat}; max-age-in-seconds=259200`;
     document.cookie = `lng=${lng}; max-age-in-seconds=259200`;
-
-
-    const key = document.cookie.split(';')[0].split('=')[1]
-    const key2 = document.cookie.split(';')[1].split('=')[1]
-    const params = 'airTemperature,windSpeed,cloudCover,humidity';
-
-    let response = await fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`, {
-        headers: {
-            'Authorization': key
-        }
-    })
-
-    if(response.status === 402) {
-        // console.log(true)
-        response = await fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`, {
-            headers: {
-                'Authorization': key2
-            }
-        })
-        // console.log('after call two')
-    }
-    const weather = await response.json();
-
-    const temp = weather.hours[0].airTemperature.noaa * 9 / 5 + 32;
-    const cloud = weather.hours[0].cloudCover.noaa
-    const wind = Math.round(weather.hours[0].windSpeed.noaa * 2.236936);
-
-    const cloudDiv = document.getElementById('cloud');
-    const windDiv = document.getElementById('wind');
-    const tempDiv = document.getElementById('temp');
-    const cityDiv = document.getElementById('city');
-
-    const addData = await fetch('/weather', {
-        method: 'POST',
-        body: JSON.stringify({lat, lng}),
-        headers:{
-            'Content-Type': 'application/json'
-        },
-    })
-
-    const parsedAdd = await addData.json();
-    const address = parsedAdd.results[0].formatted.split(',')[1]
-
-    cityDiv.innerHTML = ` In ${address}`
-    cloudDiv.innerHTML = ` ${cloud}% Coverage`
-    windDiv.innerHTML = ` ${wind} mph`
-    tempDiv.innerHTML = ` ${temp}`
 
 })
