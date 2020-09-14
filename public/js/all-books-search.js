730c2fe12dce1ea67d5fe8926e92d18c86539362
Miah Barnes <miahellenbarnes@gmail.com>
2020-09-14 10:55:29 -0400

    jony's fix on rachey

--- public/js/all-books-search.js
+++ public/js/all-books-search.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const details = document.querySelector("#list");
     details.addEventListener("click", async (e) => {
         e.preventDefault();
-        if (e.target.classList[e.target.classList.length - 1].match(/^(\w+)\d+/)) {
+        if (e.target.classList[e.target.classList.length - 1].match(/\d+/)) {
             console.log(e.target.classList[e.target.classList.length - 1]);
             window.location.href = `/books/${e.target.classList[e.target.classList.length - 1]}`;
         }
